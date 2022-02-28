c889be49af91194ddefded8cd829a1d2b663dd70
Giorgi Zekalashvili <giorgizek@gmail.com>
2022-02-28 15:42:08 +0400

    Update object-helper.ts

--- projects/zek/src/lib/utils/object-helper.ts
+++ projects/zek/src/lib/utils/object-helper.ts
@@ -1,4 +1,14 @@
 export class ObjectHelper {
+
+    static isObject(item: any): boolean {
+        return (item && typeof item === 'object' && !Array.isArray(item));
+    }
+
+
+    static isDefined(value: any) {
+        return typeof value !== 'undefined' && value !== null;
+    }
+
     static isEmpty(obj: any) {
         if (obj === undefined || obj === null)
             return true;
@@ -25,5 +35,24 @@ export class ObjectHelper {
 
     static isBoolean(val: any) {
         return val === false || val === true;
+    }
+
+
+    static mergeDeep(target: any, source: any): any {
+        let output = Object.assign({}, target);
+        if (this.isObject(target) && this.isObject(source)) {
+            Object.keys(source).forEach((key: any) => {
+                if (this.isObject(source[key])) {
+                    if (!(key in target)) {
+                        Object.assign(output, { [key]: source[key] });
+                    } else {
+                        output[key] = this.mergeDeep(target[key], source[key]);
+                    }
+                } else {
+                    Object.assign(output, { [key]: source[key] });
+                }
+            });
+        }
+        return output;
     }
 }
