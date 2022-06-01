fa1dae0fd74fc33faaafb22372c9918b869df76f
Giorgi Zekalashvili <giorgizek@gmail.com>
2022-06-01 19:40:57 +0400

    Added nav model

--- projects/zek/src/lib/models/index.ts
+++ projects/zek/src/lib/models/index.ts
@@ -9,9 +9,11 @@ export * from './key-pair.model';
 export * from './list-base.model';
 export * from './login.model';
 export * from './month.model';
+export * from './nav.model';
 export * from './pager.model';
 export * from './print.model';
 export * from './tree.model';
 export * from './valid-event-args.model';
+
 
 //export * from './';
---
+++ projects/zek/src/lib/models/nav.model.ts
@@ -0,0 +1,16 @@
+export interface Nav {
+    id?: string | null;
+    name?: string | null;
+    children: NavItem[]
+}
+export interface NavItem {
+    name?: string | null;
+    top?: boolean | null;
+    icon?: string | null;
+    badge?: string | null;
+    path?: string | null;
+    data?: {
+        roles?: [] | null,
+        permission?: { key: number } | null
+    } | null
+}
