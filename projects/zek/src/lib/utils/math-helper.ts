620bc71459d8f839b1c924c10cd9274d4959dfd2
Giorgi Zekalashvili <giorgizek@gmail.com>
2023-08-01 17:32:02 +0400

    Math clamp

--- projects/zek/src/lib/utils/math-helper.ts
+++ projects/zek/src/lib/utils/math-helper.ts
@@ -2,4 +2,9 @@ export class MathHelper {
     static round(value: number, decimals: number = 0): number {
         return Math.round(Number(value) * Math.pow(10, decimals)) / (Math.pow(10, decimals));
     };
+
+
+    static clamp(v: number, min: number, max: number) {
+        return Math.max(min, Math.min(max, v));
+    }
 }
