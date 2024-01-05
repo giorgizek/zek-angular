bdb53d8a372867f08b5294707241a8b2973dc718
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-01-05 19:58:53 +0400

    randomInt

--- projects/zek/src/lib/utils/random-helper.ts
+++ projects/zek/src/lib/utils/random-helper.ts
@@ -7,4 +7,11 @@ export class RandomHelper {
         }
         return out;
     }
+
+    static randomInt(min: number, max: number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+
+        return  Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 }
