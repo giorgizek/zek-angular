aa2ce978e5452665015f1b66da5c7de8904755dc
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-03-28 11:24:33 +0400

    updated angular

--- projects/zek/README.md
+++ projects/zek/README.md
@@ -1,6 +1,6 @@
 # Zek
 
-This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.0.
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.0.
 
 ## Code scaffolding
 
--- projects/zek/src/lib/models/pager.model.ts
+++ projects/zek/src/lib/models/pager.model.ts
@@ -3,15 +3,15 @@
 }
 
 export class Pager extends PagerBase {
-    pageCount: number = 1;
-    pageNumber: number = 1;
-    pageSize: number = 25;
-    hasPreviousPage: boolean = false;
-    hasNextPage: boolean = false;
-    isFirstPage: boolean = false;
-    isLastPage: boolean = false;
-    firstItemOnPage: number = 0;
-    lastItemOnPage: number = 0;
+    pageCount = 1;
+    pageNumber = 1;
+    pageSize = 25;
+    hasPreviousPage = false;
+    hasNextPage = false;
+    isFirstPage = false;
+    isLastPage = false;
+    firstItemOnPage = 0;
+    lastItemOnPage = 0;
 
     pages: number[] = [];
 }
@@ -21,18 +21,18 @@ export class PagedListBase<T = any> {
 
     pager: Pager = new Pager();
 
-    totalItemCount: number = 0;
-    pageNumber: number = 1;
-    pageSize: number = 25;
-    count: number = 0;
+    totalItemCount = 0;
+    pageNumber = 1;
+    pageSize = 25;
+    count = 0;
 }
 export class PagedList<T = any> extends PagedListBase<T>
 {
-    pageCount: number = 1;
-    hasPreviousPage: boolean = false;
-    hasNextPage: boolean = false;
-    isFirstPage: boolean = false;
-    isLastPage: boolean = false;
-    firstItemOnPage: number = 0;
-    lastItemOnPage: number = 0;
+    pageCount = 1;
+    hasPreviousPage = false;
+    hasNextPage = false;
+    isFirstPage = false;
+    isLastPage = false;
+    firstItemOnPage = 0;
+    lastItemOnPage = 0;
 }
