5a5873c2f636c3bc994540a03573317a900b32a7
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-09-11 18:41:30 +0400

    CacheHelper

---
+++ projects/zek/src/lib/services/cache-helper.ts
@@ -0,0 +1,47 @@
+import { Observable, of, tap } from "rxjs";
+
+interface CacheContent {
+    expiry: number;
+    value: any;
+}
+
+export class CacheHelper {
+    private static _cache = new Map<string, CacheContent>();
+
+    static get(key: string): Observable<any> | undefined {
+        const data = this._cache.get(key);
+        if (!data) {
+            return undefined;
+        }
+
+        const now = new Date().getTime();
+        if (now > data.expiry) {
+            this._cache.delete(key);
+            return undefined;
+        }
+
+        return of(data.value);
+    }
+
+    // Set data to cache
+    static set(key: string, value: any, minutes: number = 5): Observable<any> {
+        const expiry = new Date().getTime() + minutes * 60000;
+        this._cache.set(key, { expiry, value });
+        return of(value);
+    }
+
+    // Cache and return the Observable
+    static getOrCreate(key: string, fallback: Observable<any>, minutes?: number): Observable<any> {
+        const cached = this.get(key);
+        console.log(key, cached);
+        if (cached) {
+            return cached;
+        } else {
+            return fallback.pipe(
+                tap(value => {
+                    this.set(key, value, minutes);
+                })
+            );
+        }
+    }
+}
--- projects/zek/src/lib/services/index.ts
+++ projects/zek/src/lib/services/index.ts
@@ -1,6 +1,7 @@
 export * from './auth.service';
 export * from './auth-guard.service';
 export * from './base.service';
+export * from './cache-helper';
 export * from './crud.service';
 export * from './http-error-handler.service';
 export * from './web.api';
