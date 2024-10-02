b71294a5208a9afdae15ccf3798c9ee18160e917
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-10-02 19:16:54 +0400

    loadCss

--- projects/zek/src/lib/utils/html-helper.ts
+++ projects/zek/src/lib/utils/html-helper.ts
@@ -71,4 +71,13 @@ export class HtmlHelper {
         });
     }
 
+
+    static loadCss(url: string) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
 }
