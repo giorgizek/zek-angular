9be1ec83166cca2ba9b605aa595fce7b7440e33f
Giorgi Zekalashvili <giorgizek@gmail.com>
2024-11-22 17:43:07 +0400

    downloadFile

--- projects/zek/src/lib/services/file.service.ts
+++ projects/zek/src/lib/services/file.service.ts
@@ -3,6 +3,7 @@ import { catchError } from 'rxjs';
 import { CrudService } from './crud.service';
 import { WebApiClient } from './web.api';
 import { HttpErrorHandler } from './http-error-handler.service';
+import { IFileBase } from '../models';
 
 @Injectable()
 export class FileService extends CrudService {
@@ -16,5 +17,14 @@ export class FileService extends CrudService {
 
     presignedUrl(id: any, hash: any) {
         return this.api.get(`api/${this.controller}/${id}/${hash}/presigned-url`).pipe(catchError(this.handleError(this.presignedUrl.name, null)));
+    }
+
+    downloadFile(file: IFileBase) {
+        if (!file) return;
+        this.presignedUrl(file.id, file.hash).subscribe(data => {
+            if (data?.success) {
+                window.open(data.value, '_blank');
+            }
+        });
     }
 }
