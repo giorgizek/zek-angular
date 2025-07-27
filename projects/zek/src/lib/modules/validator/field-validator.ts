308ab8ada51004b54d8282898430ac66fec96b9e
Giorgi Zekalashvili <47349388+giorgizek@users.noreply.github.com>
2025-07-27 12:45:16 +0400

    field-validator

--- projects/zek/src/lib/modules/validator/field-validator.ts
+++ projects/zek/src/lib/modules/validator/field-validator.ts
@@ -3,12 +3,22 @@ import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-    standalone:true,
+    standalone: true,
     selector: 'zek-field-validator, [zek-field-validator]',
-    host: { '[class]': '"invalid-tooltip"' },
+    host: {
+        '[class.invalid-tooltip]': 'hasErrors'
+    },
     templateUrl: './field-validator.html',
     imports: [CommonModule, TranslateModule]
 })
 export class ZekFieldValidator {
     @Input() field: any;
+
+
+    get hasErrors(): boolean {
+        return !!this.field &&
+            this.field.invalid &&
+            this.field.errors &&
+            (this.field.dirty || this.field.touched || this.field.formDirective?.submitted);
+    }
 }
