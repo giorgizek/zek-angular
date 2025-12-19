62c6a46f2c3070fb10dc9dab345407815ea33342
Giorgi Zekalashvili <giorgizek@gmail.com>
2025-12-19 10:20:09 +0400

    field validator

--- projects/zek/src/lib/modules/validator/field-validator.ts
+++ projects/zek/src/lib/modules/validator/field-validator.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, Optional } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { BooleanInput } from '../../components';
+import { Convert } from '../../utils';
+import { NgForm, FormGroupDirective } from '@angular/forms';
 
 @Component({
     standalone: true,
@@ -14,11 +17,41 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ZekFieldValidator {
     @Input() field: any;
 
+    private _log = false;
+    get log(): boolean {
+        return this._log;
+    }
+    @Input()
+    set log(value: BooleanInput) {
+        const v = Convert.toBooleanProperty(value);
+        if (this._log !== v) {
+            this._log = v;
+        }
+    }
+
+    constructor(
+        @Optional() private parentForm: NgForm,
+        @Optional() private parentFormGroup: FormGroupDirective
+    ) { }
+
 
     get hasErrors(): boolean {
-        return !!this.field &&
-            this.field.invalid &&
-            this.field.errors &&
-            (this.field.dirty || this.field.touched || this.field.formDirective?.submitted);
+        if (!this.field) return false;
+
+        const errors = this.field.errors;
+        const invalid = this.field.invalid;
+
+        // Check for specific error object existence
+        if (!invalid || !errors) return false;
+
+        // 1. Check if field is touched or dirty
+        const isInteract = this.field.dirty || this.field.touched;
+
+        // 2. Check if parent form is submitted (Works for both Reactive and Template)
+        const isSubmitted = (this.parentForm && this.parentForm.submitted) ||
+            (this.parentFormGroup && this.parentFormGroup.submitted) ||
+            (this.field.formDirective && this.field.formDirective.submitted);
+
+        return isInteract || isSubmitted;
     }
 }
